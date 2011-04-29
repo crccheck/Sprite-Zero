0dabf766ebedf60d8d2979a6bf525993dfbd86ab
crccheck <c@crccheck.com>
2011-04-29 00:20:24 -0500

    refactor as a class

--- spritezero/spritezero.py
+++ spritezero/spritezero.py
@@ -11,99 +11,103 @@ import Image
 
 parser = OptionParser()
 
-PADDING = 10
+class SpriteZero:
+    def __init__(self):
+        self.PADDING = 10
+        self.PATTERN = r'url\([\'"]?([^\'"]+)[\'"]?\).*?(0|-?\d+px)\s+(0|-?\d+px)'
+        self.lookup = {}
 
-PATTERN = r'url\([\'"]?([^\'"]+)[\'"]?\).*?(0|-?\d+px)\s+(0|-?\d+px)'
+    def uri_to_file(self, uri):
+        """Translate the web root uri to local path.
 
+        TODO If uri is a url, open a file handler to the resource
 
-def uri_to_file(uri):
-    """Translate the web root uri to local path.
+        """
+        if uri[0] == '/':
+            return uri[1:]
+        elif re.match(r"http", uri):
+            # TODO close files
+            return urlopen(uri)
+        else:
+            raise NotImplementedError("Can't handle uri of type: url(%s) yet" % uri)
 
-    TODO If uri is a url, open a file handler to the resource
+    def get_dimensions(self, image_file):
+        image = Image.open(image_file)
+        return image.size
 
-    """
-    if uri[0] == '/':
-        return uri[1:]
-    elif re.match(r"http", uri):
-        # TODO close files
-        return urlopen(uri)
-    else:
-        raise NotImplementedError("Can't handle uri of type: url(%s) yet" % uri)
-
-def get_dimensions(image_file):
-    image = Image.open(image_file)
-    return image.size
-
-def sprite_for_file(f):
-    def lookup_to_list(lookup):
-        out = []
-        for key, value in lookup.iteritems():
-            value['uri'] = key
-            out.append(value)
-        return out
-    def replace_css(matchobj):
+    def replace_css(self, matchobj):
+        """replacement function that returns sprite file and proper offsets"""
         groups = matchobj.groups()
-        replacement = lookup[groups[0]]
+        replacement = self.lookup[groups[0]]
         x = int(re.sub(r"\D+", "", groups[1])) - replacement['offset'][0]
         y = int(re.sub(r"\D+", "", groups[2])) - replacement['offset'][1]
-        return "url(/%s) no-repeat scroll %dpx %dpx" % (sprite_png, x, y)
+        return "url(/%s) no-repeat scroll %dpx %dpx" % (self.sprite_png, x, y)
 
-    lookup = {}
-    pattern = PATTERN
-    for line in f:
-        match = re.search(pattern, line)
-        if not match:
-            continue
-        groups = match.groups()
-        if groups[0].find('sprite') != -1:
-            # yo dawg, i heard you like sprite so i sprited a sprite and it broke
-            continue
-        if groups[0] not in lookup:
-            print groups
-            size = get_dimensions(uri_to_file(groups[0]))
-            lookup[groups[0]] = {'size': size,
-                                 'length': size[0] * size[1]}
+    def sprite_for_file(self, f):
+        def lookup_to_list(lookup):
+            out = []
+            for key, value in lookup.iteritems():
+                value['uri'] = key
+                out.append(value)
+            return out
 
-    lookup_list = lookup_to_list(lookup)
-    lookup_list.sort(key=lambda x: x['length'])
+        pattern = self.PATTERN
+        for line in f:
+            match = re.search(pattern, line)
+            if not match:
+                continue
+            groups = match.groups()
+            if groups[0].find('sprite') != -1:
+                # yo dawg, i heard you like sprite so i sprited a sprite and it broke
+                continue
+            if groups[0] not in self.lookup:
+                size = self.get_dimensions(self.uri_to_file(groups[0]))
+                self.lookup[groups[0]] = {'size': size,
+                                          'length': size[0] * size[1]}
 
-    width = max([x['size'][0] for x in lookup.values()])     # max width
-    height = sum([x['size'][1] for x in lookup.values()]) + PADDING * len(lookup)
+        lookup_list = lookup_to_list(self.lookup)
+        lookup_list.sort(key=lambda x: x['length'])
 
-    sprite = Image.new("RGBA", (width, height))
-    last_y = 0
-    for x in lookup_list:
-        image = Image.open(uri_to_file(x['uri']))
-        offset = (0, last_y)
-        sprite.paste(image, offset)
-        lookup[x['uri']]['offset'] = offset
-        last_y += image.size[1] + PADDING
+        width = max([x['size'][0] for x in self.lookup.values()])    # max width
+        height = sum([x['size'][1] for x in self.lookup.values()]) + \
+                 self.PADDING * len(self.lookup)
 
-    sprite.save(sprite_png, "PNG")
+        sprite = Image.new("RGBA", (width, height))
+        last_y = 0
+        for x in lookup_list:
+            image = Image.open(self.uri_to_file(x['uri']))
+            offset = (0, last_y)
+            sprite.paste(image, offset)
+            self.lookup[x['uri']]['offset'] = offset
+            last_y += image.size[1] + self.PADDING
 
-    new_f = open(replacement_css, "w")
-    f.seek(0)
-    for line in f:
-        try:
-            newline = re.sub(pattern, replace_css, line)
-        except KeyError:
-            newline = line
-        new_f.write(newline)
-        if newline != line:
-            print line, newline
-            print "\n"
-    new_f.close()
+        sprite.save(self.sprite_png, "PNG")
 
-def replace_old_css():
-    """Replace the old css file with the new one"""
-    os.remove(original_css)
-    os.rename(replacement_css, original_css)
+        new_f = open(replacement_css, "w")
+        f.seek(0)
+        for line in f:
+            try:
+                newline = re.sub(pattern, self.replace_css, line)
+            except KeyError:
+                newline = line
+            new_f.write(newline)
+            if newline != line:
+                print line, newline
+                print "\n"
+        new_f.close()
+
+    def replace_old_css(self):
+        """Replace the old css file with the new one"""
+        os.remove(original_css)
+        os.rename(replacement_css, original_css)
 
 if __name__ == "__main__":
     options, args = parser.parse_args()
     original_css = args[0]
     replacement_css = original_css + ".tmp"
-    sprite_png = original_css + "-sprite.png"
     with open(original_css, "r") as f:
-        sprite_for_file(f)
-    replace_old_css()
+        converter = SpriteZero()
+        converter.sprite_png = original_css + "-sprite.png"
+        converter.sprite_for_file(f)
+    converter.replace_old_css()
+
--- tests.py
+++ tests.py
@@ -3,75 +3,101 @@ import re
 
 from spritezero import *
 
+
 class TestRegExpPattern(unittest.TestCase):
+    def setUp(self):
+        self.converter = SpriteZero()
+        self.pattern = self.converter.PATTERN
+
     def test_full_with_color_last(self):
         line = "background: url(a/b/c.png) no-repeat scroll 1px 2px #000;"
-        match = re.search(PATTERN, line)
+        match = re.search(self.pattern, line)
         self.assertEqual(('a/b/c.png', '1px', '2px'), match.groups())
 
     def test_full_with_color_first(self):
         line = "background: #000 url(a/b/c.png) no-repeat scroll 1px 2px;"
-        match = re.search(PATTERN, line)
+        match = re.search(self.pattern, line)
         self.assertEqual(('a/b/c.png', '1px', '2px'), match.groups())
 
     def test_full_with_no_color(self):
         line = "background: url(a/b/c.png) no-repeat scroll 1px 2px;"
-        match = re.search(PATTERN, line)
+        match = re.search(self.pattern, line)
         self.assertEqual(('a/b/c.png', '1px', '2px'), match.groups())
 
     def test_basic(self):
         # missing no-repeat but that's not our fault
         line = "background: url(a/b/c.png) 1px 2px;"
-        match = re.search(PATTERN, line)
+        match = re.search(self.pattern, line)
         self.assertEqual(('a/b/c.png', '1px', '2px'), match.groups())
 
     def test_ignores_other_lines(self):
         line = "background-image: url(a/b/c.png)"
-        match = re.search(PATTERN, line)
+        match = re.search(self.pattern, line)
         self.assertEqual(None, match)
 
     def test_can_handle_quotes(self):
         #using mixed quotes here to do two tests in one
         line = "background: url(\"a/b/c.png') no-repeat scroll 1px 2px #000;"
-        match = re.search(PATTERN, line)
+        match = re.search(self.pattern, line)
         self.assertEqual(('a/b/c.png', '1px', '2px'), match.groups())
 
     def test_negative_offsets(self):
         #using mixed quotes here to do two tests in one
         line = "background: url(\"a/b/c.png') no-repeat scroll -1px -2px #000;"
-        match = re.search(PATTERN, line)
+        match = re.search(self.pattern, line)
         self.assertEqual(('a/b/c.png', '-1px', '-2px'), match.groups())
 
     def test_skip_imports(self):
         line = '@import url("import.css");'
-        match = re.search(PATTERN, line)
+        match = re.search(self.pattern, line)
         self.assertEqual(None, match)
 
+class TestRegExpPatternReplacement(unittest.TestCase):
+    def setUp(self):
+        self.converter = SpriteZero()
+        self.pattern = self.converter.PATTERN
+        self.converter.sprite_png = 'sprite.png'
+
+    def test_trivial_offset(self):
+        self.converter.lookup = {'a/b/c.png': {'offset': [0, 0]}}
+        line = "background: url(a/b/c.png) no-repeat scroll 1px 2px;"
+        self.assertEqual(re.sub(self.pattern, self.converter.replace_css, line),
+            'background: url(/sprite.png) no-repeat scroll 1px 2px;');
+
+    def test_can_set_offset(self):
+        self.converter.lookup = {'a/b/c.png': {'offset': [2, 1]}}
+        line = "background: url(a/b/c.png) no-repeat scroll 1px 2px;"
+        self.assertEqual(re.sub(self.pattern, self.converter.replace_css, line),
+            'background: url(/sprite.png) no-repeat scroll -1px 1px;');
+
 class TestURIReading(unittest.TestCase):
+    def setUp(self):
+        self.converter = SpriteZero()
+
     def test_read_absolute_path(self):
         uri = "/images/someimage.png"
-        self.assertEqual(uri_to_file(uri), "images/someimage.png")
+        self.assertEqual(self.converter.uri_to_file(uri), "images/someimage.png")
 
     def test_read_http(self):
         uri = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/themes/trontastic/images/ui-bg_diagonals-small_50_262626_40x40.png"
-        f = uri_to_file(uri)
+        f = self.converter.uri_to_file(uri)
         f.read(1)   # seek to 1
         self.assertEqual(f.read(3), "PNG")
 
     def test_read_https(self):
         uri = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/themes/trontastic/images/ui-bg_diagonals-small_50_262626_40x40.png"
-        f = uri_to_file(uri)
+        f = self.converter.uri_to_file(uri)
         f.read(1)   # seek to 1
         self.assertEqual(f.read(3), "PNG")
 
     def test_404(self):
         from urllib2 import HTTPError
         uri = "http://localhost/lemonparty.jpg"
-        self.assertRaises(HTTPError, uri_to_file, uri)
+        self.assertRaises(HTTPError, self.converter.uri_to_file, uri)
 
     def test_not_implemented(self):
         uri = "../goatse.jpg"
-        self.assertRaises(NotImplementedError, uri_to_file, uri)
+        self.assertRaises(NotImplementedError, self.converter.uri_to_file, uri)
 
 if __name__ == '__main__':
     unittest.main()
